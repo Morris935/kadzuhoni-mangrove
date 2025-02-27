c4b8c05d4bae1a3aa1f6bc1ed1cf66caf2fcfea0
Morris935 <stoneyabdul@gmail.com>
2025-02-28 00:42:19 +0300

    Overwriting existing Django project with modified local version

--- mangrove_project/mangrove/admin.py
+++ mangrove_project/mangrove/admin.py
@@ -1,3 +1,65 @@
 from django.contrib import admin
+from .models import Member, Transaction, Planting, Event, Document, Expense, Sale
+
 
 # Register your models here.
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = [
+        'membership_id', 'first_name', 'second_name', 'other_name', 'gender', 'phone',
+        'designation','registration_date', 'status'
+    ]
+    list_filter = [
+        'gender', 'registration_date', 'status'
+    ]
+    search_fields = ['membership_id', 'first_name', 'second_name', 'other_name']
+    # raw_id_fields = ['membership_id']
+    # date_hierarchy = ['registration_date']
+    # ordering = ['subscription_status', 'subscription_date']
+    show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['member', 'amount', 'transaction_type', 'description', 'transaction_date']
+    list_filter = ['transaction_type', 'transaction_date']
+    search_fields = ['memmber', 'transaction_type']
+    # date_hierarchy = ['transaction_date']
+    show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Planting)
+class PlantingAdmin(admin.ModelAdmin):
+    list_display = ['date', 'seedlings']
+    list_filter = ['date']
+    search_fields = ['date']
+    show_facets = admin.ShowFacets.ALWAYS
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'start_date', 'end_date', 'location', 'status','created_by', 'date_created']
+    list_filter = ['start_date', 'status', 'date_created']
+    search_fields = ['title', 'location', 'status', 'created_by']
+    show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['description', 'category', 'amount', 'date_incurred', 'incurred_by', 'date_logged']
+    list_filter = ['category', 'date_incurred', 'incurred_by']
+    search_fields = ['description', 'category', 'incurred_by']
+    show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'file_path', 'uploaded_by', 'date_uploaded', 'access_level']
+    list_filter = ['date_uploaded', 'access_level']
+    search_fields = ['title']
+    # list_display_links = ['file_path']
+    show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['product', 'quantity', 'unit_price', 'buyer', 'sale_date']
+    list_filter = ['sale_date']
+    search_fields = ['product', 'buyer']
+    # list_display_links = ['file_path']
+    show_facets = admin.ShowFacets.ALWAYS
--- mangrove_project/mangrove/models.py
+++ mangrove_project/mangrove/models.py
@@ -1,3 +1,118 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Member(models.Model):
+    GENDER = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+    )
+    membership_id = models.CharField(max_length=50, unique=True, default="KMC")
+    first_name = models.CharField(max_length=150)
+    second_name = models.CharField(max_length=150, null=True, blank=True)
+    other_name = models.CharField(max_length=50, null=True, blank=True)
+    gender = models.CharField(max_length=50, choices=GENDER, default='male')
+    phone = models.CharField(max_length=13, blank=True, null=True)
+    email = models.EmailField(max_length=150, blank=True, null=True)
+    designation = models.CharField(max_length=100, default='member')
+    registration_date = models.DateTimeField(default=timezone.now, blank=True)
+    status = models.CharField(max_length=50, default="active")
+    # subscription_date = models.DateTimeField(default=timezone.now, blank=True)
+
+    class Meta:
+        ordering = ['-registration_date']
+        indexes = [
+            models.Index(fields=['-registration_date'])
+        ]
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.second_name}"
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('registration', 'Registration'),
+        ('contribution', 'Contribution'),
+        ('subscription', 'Subscription'),
+    )
+    member = models.ForeignKey("Member",on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=50, choices=TRANSACTION_TYPES)
+    description = models.TextField()
+    transaction_date = models.DateTimeField(auto_now_add=False)
+
+    class Meta:
+        ordering = ['-transaction_date']
+        indexes = [
+            models.Index(fields=['-transaction_date'])
+        ]
+
+
+    def __str__(self):
+        return f"{self.member.first_name} - {self.transaction_type} - {self.amount}"
+    
+class Planting(models.Model):
+    date = models.DateTimeField(auto_now_add=False)
+    # planted = models.IntegerField(blank=True, null=True)
+    seedlings = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.date} - Seedlings {self.seedlings}"
+    
+class Event(models.Model):
+    STATUS_CHOICES = (
+        ('upcoming', 'Upcoming'),
+        ('ongoing', 'Ongoing'),
+        ('completed', 'Completed'),
+    )
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True, null=True)
+    location = models.CharField(max_length=250)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='upcoming')
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_events')
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.status}"
+    
+
+class Document(models.Model):
+    ACCESS_LEVEL_CHOICES = (
+        ('public', 'Public'),
+        ('member', 'Member'),
+        ('admin', 'Admin'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    file_path = models.FileField(upload_to='documents/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    access_level = models.CharField(max_length=50, choices=ACCESS_LEVEL_CHOICES, default='member')
+
+    def __str__(self):
+        return self.title
+
+class Expense(models.Model):
+    description = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_incurred = models.DateTimeField()
+    incurred_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date_logged = models.DateTimeField(auto_now_add=False)
+
+    def __str__(self) -> str:
+        return f"{self.category} - {self.amount}"
+    
+class Sale(models.Model):
+    product = models.CharField(max_length=150)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    buyer = models.CharField(max_length=150)
+    sale_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.product} - {self.quantity * self.unit_price}"
